3f22edab6fb2668c54d292f85f58d5ed005b7591
nju33 <nju33.ki@gmail.com>
2018-11-20 16:37:09 +0900

    fix: remove import statement for removed file

--- src/slack-hooks/index.ts
+++ src/slack-hooks/index.ts
@@ -1,6 +1,5 @@
 export * from './use-attachment';
 export * from './use-dialog';
-export * from './use-element';
 export * from './use-field';
 export * from './use-message';
 export * from './use-option-group';

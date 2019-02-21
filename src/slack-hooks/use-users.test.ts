36c1e94ac35f309880b276f114035302690ad122
nju33 <nju33.ki@gmail.com>
2019-02-21 16:01:24 +0900

    fix(users): write test

--- src/slack-hooks/use-users.test.ts
+++ src/slack-hooks/use-users.test.ts
@@ -2,12 +2,21 @@ import {useUsers} from './use-users';
 
 describe('useUsers', () => {
   test('returns', () => {
-    const {json, setName, setProfile, setUser, setValue} = useUsers();
+    const {json, setName, setProfile, setUser, setValue} = useUsers('profile');
 
     expect(json).toBeInstanceOf(Function);
     expect(setName).toBeInstanceOf(Function);
     expect(setProfile).toBeInstanceOf(Function);
     expect(setUser).toBeInstanceOf(Function);
     expect(setValue).toBeInstanceOf(Function);
+
+    setName('foo');
+    setUser('A');
+    setValue('yes');
+
+    const result: any = json(false);
+    expect(result.name).toBe('foo');
+    expect(result.user).toBe('A');
+    expect(result.value).toBe('yes');
   });
 });
--- src/slack-hooks/use-users.ts
+++ src/slack-hooks/use-users.ts
@@ -1,8 +1,12 @@
 import {Users} from '../structs/users';
 
+type UsersType = 'profile';
+
 // tslint:disable-next-line:no-namespace
 namespace UseUsers {
-  export function fn(): {
+  export function fn(
+    type: UsersType,
+  ): {
     json: Users['export'];
     setName: Users['setName'];
     setProfile: Users['setProfile'];
@@ -10,16 +14,23 @@ namespace UseUsers {
     setValue: Users['setValue'];
   };
 
-  export function fn() {
+  export function fn(type: UsersType) {
     const users = new Users();
 
-    return {
-      json: users.export.bind(users),
-      setName: users.setName,
-      setProfile: users.setProfile,
-      setUser: users.setUser,
-      setValue: users.setValue,
-    };
+    switch (type) {
+      case 'profile': {
+        return {
+          json: users.export.bind(users),
+          setName: users.setName,
+          setProfile: users.setProfile,
+          setUser: users.setUser,
+          setValue: users.setValue,
+        };
+      }
+      default: {
+        return {};
+      }
+    }
   }
 }
 
